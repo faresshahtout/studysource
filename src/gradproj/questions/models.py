88264f2ca2faea5316935891624df7230f4af3c6
fares shahtout <anzifares@gmail.com>
2022-05-04 16:01:05 +0300

    committing changes to the sqlite functions

--- src/gradproj/questions/models.py
+++ src/gradproj/questions/models.py
@@ -5,4 +5,4 @@ class questions(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question_id=models.IntegerField(default=000)
     question=models.TextField(default='')
-    result=models.DecimalField(decimal_places=2,max_digits=2,null=True,)
+    result=models.CharField(default="not taking test yet",max_length=70)
--- src/gradproj/questions/views.py
+++ src/gradproj/questions/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 import pandas as p
 from sklearn.tree import DecisionTreeClassifier
+from django.contrib import messages
 import joblib as j
-
+from .models import questions
 # Create your views here.
 from django.shortcuts import  render, redirect
 def questions_view(request):
+    p=''
     dir=request.POST
     new_list=list(dir.values())
     print(new_list)
     new_list=new_list[1:]
+    if len(new_list)==19:
+        last_list = []
+        for s in new_list:
+            last_list.append(float(s))
+        print(last_list)
+        model = j.load('studysorce.joblib')
+        predictions = model.predict([last_list])
 
-    last_list=[]
-    for s in new_list:
-        last_list.append(float(s))
-    print(last_list)
-    model = j.load('studysorce.joblib')
-    predictions = model.predict([last_list])
-    for p in predictions:
-        print(p)
+        for p in predictions:
+            p
+
+        student = questions(user=request.user, result=p)
+        student.save()
+    else:
+        messages.error(request, "please fill all the required fields.")
+        # return(redirect('/questions/'))
+
+
     return render(request=request, template_name="question.html", context={})
